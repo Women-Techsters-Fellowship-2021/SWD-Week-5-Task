c7a5877b678807c72b8709df59128b4fdda48f00
lorlahdey <horlorlahdey33@gmail.com>
2021-06-19 23:12:10 +0100

    submissions for Lolade Lawal

---
+++ loladeLawal/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import TodoApp from './components/todoApp';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './App.css';
+
+
+const App = () =>  {
+
+  
+  return (
+		<BrowserRouter>
+		
+				<Navbar />
+      
+				<Switch>
+					<Route path='/login'>
+						<Login />
+					</Route>
+					<Route path='/register'>
+						<Register />
+					</Route>
+					<Route path='/todo-app'>
+						<TodoApp />
+					</Route>
+					<Route>
+						<Login />
+					</Route>
+				</Switch>
+			
+		</BrowserRouter>
+	);
+}
+
+export default App;
